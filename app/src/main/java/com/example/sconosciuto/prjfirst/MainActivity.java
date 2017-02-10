77f40ffd73916cc8cf340a303aafff892ec8ad81
ValerioTorrese <valerio.torrese@gmail.com>
2017-02-10 10:41:24 +0100

    PrjFirst

---
+++ app/src/main/java/com/example/sconosciuto/prjfirst/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.sconosciuto.prjfirst;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Sconosciuto on 08/02/2017.
+ */
+
+public class MainActivity extends Activity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
